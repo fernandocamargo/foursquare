c419f436c72456ef1cea676a1e411f07319c28cf
Fernando Camargo <fernando.camargo@sellics.com>
2019-10-12 17:51:30 +0200

    Update propTypes

--- src/components/elements/participant/statics.js
+++ src/components/elements/participant/statics.js
@@ -1,14 +1,18 @@
-import { string } from 'prop-types';
+import { string, arrayOf, shape, func } from 'prop-types';
+
+import { omit } from 'helpers';
+import { propTypes as Venue } from 'components/elements/venue/statics';
 
 export const displayName = 'Participant';
 
 export const propTypes = {
   className: string.isRequired,
-  name: string,
-  vote: string,
+  id: string.isRequired,
+  name: string.isRequired,
+  vote: string.isRequired,
+  venues: arrayOf(shape(omit(Venue, 'className'))).isRequired,
+  onIdentify: func.isRequired,
+  onVote: func.isRequired,
 };
 
-export const defaultProps = {
-  name: '',
-  vote: '',
-};
+export const defaultProps = {};
